f382b03b3790d6ffd3f7e714fbe92e73e765f709
Fumiya Katoh <katoh.numazu.ct@gmail.com>
2014-04-01 22:21:29 +0900

    Initial Commit

---
+++ UriVisualizer/UriVisualizerDebugger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.DebuggerVisualizers;
+
+[assembly:System.Diagnostics.DebuggerVisualizer(
+	typeof(UriVisualizer.UriVisualizerDebugger),
+	typeof(VisualizerObjectSource),
+	Target = typeof(System.Uri),
+	Description = "Uri Open")]
+
+namespace UriVisualizer
+{
+    public class UriVisualizerDebugger : DialogDebuggerVisualizer
+    {
+		protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider) {
+			System.Diagnostics.Process.Start(objectProvider.GetObject().ToString());
+		}
+
+		public static void TestShowVisualizer(object objectToVisualize) {
+			var vHost = new VisualizerDevelopmentHost(objectToVisualize, typeof(UriVisualizerDebugger));
+			vHost.ShowVisualizer();
+		}
+	}
+}
---
+++ VisualizerTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UriVisualizer;
+
+namespace VisualizerTest {
+	class Program {
+		static void Main(string[] args) {
+			var hoge = @"http://kamiya.hatenadiary.jp/";
+			UriVisualizerDebugger.TestShowVisualizer(hoge);
+		}
+	}
+}
